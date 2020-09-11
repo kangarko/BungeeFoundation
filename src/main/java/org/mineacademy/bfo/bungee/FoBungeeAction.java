02e34f920f15a416a5dacc2f880d55399a15efdf
kangarko <matej@matejpacan.com>
2020-09-11 19:51:20 +0200

    Update FoBungeeAction.java

--- src/main/java/org/mineacademy/bfo/bungee/FoBungeeAction.java
+++ src/main/java/org/mineacademy/bfo/bungee/FoBungeeAction.java
@@ -21,13 +21,17 @@ public enum FoBungeeAction implements BungeeAction {
 
 	/**
 	 * Execute a command on bungee
+	 *
+	 * Parameters: command
 	 */
-	BUNGEE_COMMAND("command"),
+	BUNGEE_COMMAND(String.class),
 
 	/**
 	 * Sends the player a simple colorized message
+	 *
+	 * Parameters: player, message
 	 */
-	TELL_PLAYER("player", "message"),
+	TELL_PLAYER(String.class, String.class),
 
 	// ----------------------------------------------------------------------------------------------------
 	// Forward to Bukkit servers
@@ -35,56 +39,76 @@ public enum FoBungeeAction implements BungeeAction {
 
 	/**
 	 * Mute the chat.
+	 *
+	 * Parameters: message
 	 */
-	CHAT_MUTE("message"),
+	CHAT_MUTE(String.class),
 
 	/**
 	 * Wipe the chat.
+	 *
+	 * Parameters: message
 	 */
-	CHAT_CLEAR("message"),
+	CHAT_CLEAR(String.class),
 
 	/**
 	 * Broadcast a message in a channel.
+	 *
+	 * Parameters: sender, channel name, message, has mute bypass permission, has ignore bypass permission
 	 */
-	CHANNEL("sender", "channel_name", true /*hasMuteBypassPermission*/, "message"),
+	CHANNEL(String.class, String.class, String.class, Boolean.class, Boolean.class),
 
 	/**
 	 * Sends an announcement message to everyone, and plays a nice sound
+	 *
+	 * Parameters: console message, chat message to announce
 	 */
-	ANNOUNCEMENT("log message", "message"),
-
-	/**
-	 * Broadcast a json message to every player with permission.
-	 */
-	BROADCAST_JSON_WITH_PERMISSION("permission", "json_message"),
+	ANNOUNCEMENT(String.class, String.class),
 
 	/**
 	 * Broadcast a json message to every player with permission as a sender.
+	 *
+	 * Parameters: sender, show permission, json message, has sender ignore bypass permission?
 	 */
-	BROADCAST_JSON_WITH_PERMISSION_AS("sender", "sender_uuid", "permission", "json_message"),
+	BROADCAST_JSON_WITH_PERMISSION_AS(String.class, String.class, String.class, Boolean.class),
 
 	/**
 	 * Forwards a spy message to players with permissions
+	 *
+	 * Parameters: message
 	 */
-	SPY("message"),
+	SPY(String.class),
 
 	/**
 	 * Attempts to find a private message recipient, reports back to bungee
 	 * whether we found him or not
+	 *
+	 * Parameters: sender, recipient, sender message, recipient message, has bypass permission, has toggle PM bypass permission, has PM vanished bypass permission
 	 */
-	PM_LOOKUP("sender", "receiver", "senderRawMessage", "receiverMessage", true /*hasBypassPermission*/, true /*hasTogglePMBypassPermission*/, true /*hasPMVanishedPermission*/),
+	PM_LOOKUP(String.class, String.class, String.class, String.class, Boolean.class, Boolean.class, Boolean.class),
 
 	/**
 	 * This message is forwarded after PM_LOOKUP to the sender server
 	 * to tell him that the recipient was not found
+	 *
+	 * Parameters: sender, recipient
 	 */
-	PM_PLAYER_NOT_FOUND("sender", "playerThatIsNotFound"),
+	PM_PLAYER_NOT_FOUND(String.class, String.class),
 
 	/**
 	 * Forwarded after PM_LOOKUP is done to deliver the private message
 	 * to the recipient
+	 *
+	 * Parameters: sender, recipient, message
 	 */
-	PM_PLAYER_FOUND("sender", "recipient", "message"),
+	PM_PLAYER_FOUND(String.class, String.class, String.class),
+
+	/**
+	 * Full file data is compressed then forwarded to another server.
+	 *
+	 * Parameters: file name, file data
+	 */
+	FILE_SYNCHRONIZE(String.class, byte[].class),
 
 	;
 
